cffa3ad2a265905b5f94516734b1fe794a9ed048
JCGOMEZEBOCC <88060474+JCGOMEZES@users.noreply.github.com>
2023-05-10 12:26:25 -0500

    Update bootstrap.sh

--- Infraestructura/user-data/bootstrap.sh
+++ Infraestructura/user-data/bootstrap.sh
@@ -2,11 +2,13 @@
 set -e
 
 sudo apt update
+sudo apt install -y nginx
 sudo yum install -y docker
 sudo service docker start
 sudo chkconfig docker on 
 sudo usermod -a -G docker ec2-user
-docker run 
+docker pull juancamilodevops/imagen-apigateway:latest
+docker run juancamilodevops/imagen-apigateway  
 
 
 cat << EOF > /home/ubuntu/test.txt
